2a50d60a8aabf7f784d2e078593c9c0d5936cf64
Aiden Appleby <aiden.appleby3@gmail.com>
2022-04-22 14:08:43 -0700

    added searching

--- src/app/public/videos/videos.component.ts
+++ src/app/public/videos/videos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Video } from 'src/providers/video.provider';
 import { RequestService } from 'src/services/http/request.service';
@@ -6,16 +6,17 @@ import { RequestService } from 'src/services/http/request.service';
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
-  styleUrls: ['./videos.component.scss']
+  styleUrls: ['./videos.component.scss'],
 })
-
 export class VideosComponent implements OnInit {
+  videos: Video[] = [];
+  @ViewChild('searchInput', {'static': false}) searchInput!:ElementRef;
 
-  videos: Video[] =  []
+  bools = {
+    isSearching: false,
+  }
 
-  constructor(
-    private request: RequestService
-  ) { }
+  constructor(private request: RequestService) {}
 
   ngOnInit(): void {
     this.initialize();
@@ -23,10 +24,65 @@ export class VideosComponent implements OnInit {
 
   async initialize(): Promise<void> {
     try {
-      const response: any = await this.request.get(`${environment.API_URL}/list/videos/20`);
-      this.videos = (response as Video[]);
-    } catch(err) { 
+      let response = await this.getVideos(20, 0, '');
+      this.videos = response;
+    } catch (err) {
       console.log(err);
+    }
+  }
+  
+
+  async beginSearch(e: any): Promise<void> {
+    try {
+      let value = e.target.value.trim()
+      if (value.length > 0) {
+        // Search
+        this.bools.isSearching = true;
+        let response = await this.getVideos(20, 0, value);
+        this.videos = response;
+      } else {
+        // Clear
+        this.bools.isSearching = false
+        this.initialize();
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async loadNextSearchable(): Promise<void> {
+    try {
+      let response = await this.getVideos(20, this.videos.length, this.searchInput.nativeElement.value);
+      this.videos.push(...response)
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getNextGroup(): Promise<void> {
+    try {
+      let response = await this.getVideos(20, this.videos.length, '');
+      this.videos.push(...response)
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getVideos(
+    limit: number,
+    offset: number,
+    search: string
+  ): Promise<Video[]> {
+    try {
+      const response: any = await this.request.get(
+        `${environment.API_URL}/list/videos?limit=${limit}&offset=${offset}&search=${search}`
+      );
+      if (response == null) {
+        return [];
+      }
+      return response as Video[];
+    } catch (error) {
+      throw error;
     }
   }
 
@@ -34,5 +90,4 @@ export class VideosComponent implements OnInit {
     window.location.href = `/videos/v/${id}`;
     return;
   }
-
 }
