d3f958f7ed2f5c219c0fe921a0f46092b20e6e93
Aiden Appleby <aiden.appleby3@gmail.com>
2022-05-09 22:43:19 -0700

    added deeper playlist support

--- src/app/public/playlists/playlists.component.ts
+++ src/app/public/playlists/playlists.component.ts
@@ -1,17 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Playlist } from 'src/providers/playlist.provider';
+import { RequestService } from 'src/services/http/request.service';
 
 @Component({
   selector: 'app-playlists',
   templateUrl: './playlists.component.html',
-  styleUrls: ['./playlists.component.scss']
+  styleUrls: ['./playlists.component.scss'],
 })
 export class PlaylistsComponent implements OnInit {
+  constructor(
+    private request: RequestService
+  ) {}
 
-  constructor() { }
-
-  videos = [];
+  playlists: Playlist[] = [];
 
   ngOnInit(): void {
+    this.initialize();
   }
 
+  // Page Initializer
+
+  async initialize(): Promise<void> {
+    try {
+      this.playlists = await this.GetPlaylists();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Get Playlists from DB
+
+  async GetPlaylists(): Promise<Playlist[]> {
+    try {
+      const response = await this.request.get(
+        `${environment.API_URL}/list/playlists?limit=20&offset=0`
+      );
+      return (response.body as Playlist[])  
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Navigators
+
+  NavigateToPlaylist(plist: Playlist): void {
+    window.location.href = '/playlist/'+plist.route
+  }
 }
